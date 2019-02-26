10558ddaa92731dbac6de5b81bf78c039624bb9b
HawkBlues <HawkBlues@users.noreply.github.com>
2019-02-26 23:52:52 +0000

    Initial app upload
    Working on the registration activity

---
+++ App/app/src/main/java/com/example/planty/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.planty
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+        register_button_register.setOnClickListener {
+
+            val email = email_edittext_register.text.toString()
+            var password = password_edittext_register.text.toString()
+
+            Log.d("MainActivity", "Email is:" + email)
+            Log.d("MainActivity", "Password: " + password)
+
+            //GOT TO 23.55 in the video
+        }
+    }
+}
