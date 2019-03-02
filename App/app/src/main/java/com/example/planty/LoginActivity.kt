69d6ae0ce1cdf5e4caa069f6fb1589fa094368e8
HawkBlues <HawkBlues@users.noreply.github.com>
2019-03-02 18:48:49 +0000

    Updated MainActivity | create new users
    Updated MainActivity to connect to firebase to create new users

--- App/app/src/main/java/com/example/planty/LoginActivity.kt
+++ App/app/src/main/java/com/example/planty/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.example.planty
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() { //Combatability Activity
 
@@ -10,6 +13,19 @@ class LoginActivity: AppCompatActivity() { //Combatability Activity
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
+
+        button_login_login.setOnClickListener {
+            val email = edittext_email_login.text.toString()
+            val password = edittest_password_login.text.toString()
+        }
+
+        textview_backtoregistration_login.setOnClickListener {
+            Log.d("MainActivity", "Returning to Registration activity")
+
+            val intent = Intent(this,  MainActivity::class.java)
+            startActivity(intent)
+        }
+
 
     }
 }
--- App/app/src/main/java/com/example/planty/MainActivity.kt
+++ App/app/src/main/java/com/example/planty/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,19 +13,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
-
-
         register_button_register.setOnClickListener {
 
             val email = email_edittext_register.text.toString()
-            var password = password_edittext_register.text.toString()
+            val password = password_edittext_register.text.toString()
 
             Log.d("MainActivity", "Email is:" + email)
-            Log.d("MainActivity", "Password: " + password)
+            Log.d("MainActivity", "Password: $password")
 
-            //GOT TO 23.55 in the video
+            //Firebase authentication
+/*            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener //If it fails, return
+
+
+                //else if successful
+               Log.d("Main", "Successfully created user with uid: ${it.result.user.uid}")
+            }*/
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful){
+                        Log.d("Main", "CreateUserWithEmail: success")
+                        val user = FirebaseAuth.getInstance().currentUser
+                       Log.d("Main", "" + user)
+                    }else{
+                        //If signin fails
+                        Log.d("Main", "Sign in failed", task.exception)
+
+                    }
+                }
+
         }
 
         alreadyhaveaccount_textview_register.setOnClickListener {
