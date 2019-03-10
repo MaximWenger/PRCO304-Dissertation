1ab4809312c6540b0e5856db5d8a3f6556e284bf
HawkBlues <HawkBlues@users.noreply.github.com>
2019-03-10 18:39:41 +0000

    Updated MainActivity layout

--- App/app/src/main/java/com/example/planty/MainActivity.kt
+++ App/app/src/main/java/com/example/planty/MainActivity.kt
@@ -20,46 +20,52 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         register_button_register.setOnClickListener {
-
-            val email = email_edittext_register.text.toString() //Get the user email
-            val password = password_edittext_register.text.toString() //Get the user password
-
-        if (email.isEmpty() || password.isEmpty()){ //If both fields are empty, display error message to user
-            Toast.makeText(this, "Please enter an email and password", Toast.LENGTH_SHORT).show()
-            return@setOnClickListener
-            }
-            else if (email.isEmpty()) { //If the email is empty, display error message to user
-                Toast.makeText(this, "Please enter an email", Toast.LENGTH_SHORT).show()
-            return@setOnClickListener
-            } else if (password.isEmpty()){ //If the password is empty, display error message to user
-                Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT).show()
-            return@setOnClickListener
-            }
-
-            Log.d("MainActivity", "Email is:" + email) //DEBUG
-            Log.d("MainActivity", "Password: $password") //DEBUG
-
-
-                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener {
-                    if(!it.isSuccessful) return@addOnCompleteListener
-
-                    Log.d("Main", "Successfully created user with uid: ${it.result!!.user.uid}")
-                }
-                    .addOnFailureListener{
-                        Log.d("Main", "Failed to create user ${it.message}")
-                        Toast.makeText(this, "Failed to create. ${it.message}", Toast.LENGTH_SHORT).show()
-                    }
-
+            registerUser()
         }
 
         alreadyhaveaccount_textview_register.setOnClickListener {
-            Log.d("MainActivity", "Trying to Log in")
-
-            //Launch the login activity
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+            changeActivityToLogin()
         }
+    }
+
+    private fun changeActivityToLogin() {
+        Log.d("MainActivity", "Trying to Log in")
+        //Launch the login activity
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun registerUser(){
+
+        val email = email_edittext_register.text.toString() //Get the user email
+        val password = password_edittext_register.text.toString() //Get the user password
+
+        if (email.isEmpty() || password.isEmpty()){ //If both fields are empty, display error message to user
+            Toast.makeText(this, "Please enter an email and password", Toast.LENGTH_SHORT).show()
+            return
+        }
+        else if (email.isEmpty()) { //If the email is empty, display error message to user
+            Toast.makeText(this, "Please enter an email", Toast.LENGTH_SHORT).show()
+            return
+        } else if (password.isEmpty()){ //If the password is empty, display error message to user
+            Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d("MainActivity", "Email is:" + email) //DEBUG
+        Log.d("MainActivity", "Password: $password") //DEBUG
+
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if(!it.isSuccessful) return@addOnCompleteListener
+
+                Log.d("Main", "Successfully created user with uid: ${it.result!!.user.uid}")
+            }
+            .addOnFailureListener{
+                Log.d("Main", "Failed to create user ${it.message}")
+                Toast.makeText(this, "Failed to create. ${it.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 
 }
