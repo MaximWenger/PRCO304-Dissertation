5e3743c1616510a9fa25852b53e8499b396dced7
HawkBlues <HawkBlues@users.noreply.github.com>
2019-04-01 00:43:49 +0100

    Updated IdentifyActivity
    Can now view confidence results to 2dp

--- App/app/src/main/java/com/example/planty/Activities/IdentifiedActivity.kt
+++ App/app/src/main/java/com/example/planty/Activities/IdentifiedActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.planty.R
 import com.google.firebase.auth.FirebaseAuth
@@ -11,6 +12,12 @@ import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_identified.*
+import kotlinx.android.synthetic.main.identifiedrow.*
+import java.lang.Exception
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import java.util.ArrayList
 
 class IdentifiedActivity : AppCompatActivity() {
 
@@ -18,26 +25,114 @@ class IdentifiedActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_identified)
 
-        supportActionBar?.title ="Identified"
-
-        val adapter = GroupAdapter<ViewHolder>()
-
-        adapter.add(IdentifiedPlant())
-        adapter.add(IdentifiedPlant())
-        adapter.add(IdentifiedPlant())
-
-        recyclerView_Identified.adapter
-
+        supportActionBar?.title ="Identified" //change activity Title
 
         verifyLoggedIn()
-
-        val test = intent.getStringArrayListExtra("identifications")
-        Log.d("IdentifiedActivity", "test = ${test.size}")
-        for (item in test){
-            Log.d("IdentifiedActivity", "test = ${item}")
-        }
-        Log.d("IdentifiedActivity", "COMPLETED")
+try {
+    val identifiedString = intent.getStringArrayListExtra("identifications")
+    populateIDRows(identifiedString)
+}
+catch (e: Exception){
+    Log.d("IdentifiedActivity", "Create Error = ${e.message}")
+}
     }
+
+    private fun populateIDRows(identifiedString: ArrayList<String>){//Populate the text fields
+        if (identifiedString.size == 3) { //If only one identification
+            Log.d("IdentifiedActivity", "Got to 3")
+            populateIdent0(identifiedString)
+            hideIdent1()
+            hideIdent2()
+        }
+        else if(identifiedString.size == 6){ //If only two identifications
+            Log.d("IdentifiedActivity", "Got to 6")
+            populateIdent0(identifiedString)
+            populateIdent1(identifiedString)
+            hideIdent2()
+        }
+        else{ //Show the top three identifications
+            Log.d("IdentifiedActivity", "Got to ELSE")
+            populateIdent0(identifiedString)
+            populateIdent1(identifiedString)
+            populateIdent2(identifiedString)
+        }
+    }
+
+    private fun hideIdent1(){ //Hide the textviews and button
+      identified_button1.visibility = View.INVISIBLE
+      identified_name_textview1.visibility = View.INVISIBLE
+        identified_confidence1.visibility = View.INVISIBLE
+    }
+
+    private fun hideIdent2(){//Hide the textviews and button
+        identified_button2.visibility = View.INVISIBLE
+        identified_name_textview2.visibility = View.INVISIBLE
+        identified_confidence2.visibility = View.INVISIBLE
+    }
+
+    private fun populateIdent0(identifiedString: ArrayList<String>){//Populate the textfields with the identification information
+        try {
+            var singleIdent = getSingleIdent(identifiedString, 0)//Make a local copy of the first three items
+            var confidence = convertTo2dp(singleIdent.get(0))//Convert string to 2dp
+            identified_name_textview0.text = singleIdent.get(2)
+            identified_confidence0.text = confidence
+        }
+        catch(e: Exception){
+            Log.d("IdentifiedActivity", "PopulateIdent0 Error = ${e.message}")
+        }
+    }
+
+    private fun populateIdent1(identifiedString: ArrayList<String>){//Populate the textfields with the identification information
+        try {
+            var singleIdent = getSingleIdent(identifiedString, 3)
+            var confidence = convertTo2dp(singleIdent.get(0))//Convert string to 2dp
+            identified_name_textview1.text = singleIdent.get(2)
+            identified_confidence1.text = confidence
+        }catch (e: Exception){
+            Log.d("IdentifiedActivity", "PopulateIdent1 Error = ${e.message}")
+        }
+    }
+
+    private fun populateIdent2(identifiedString: ArrayList<String>){//Populate the textfields with the identification information
+        try {
+            var singleIdent = getSingleIdent(identifiedString, 6)
+            var confidence = convertTo2dp(singleIdent.get(0))//Convert string to 2dp
+            identified_name_textview2.text = singleIdent.get(2)
+            identified_confidence2.text = confidence
+        }
+        catch (e: Exception){
+            Log.d("IdentifiedActivity", "PopulateIdent2 Error = ${e.message}")
+        }
+    }
+
+    private fun convertTo2dp(string: String): String{ //Converts a decimal to 2dp, returning a string
+        var origString = string.toDouble()
+        val newString = BigDecimal(origString).setScale(2, RoundingMode.HALF_EVEN)//Round the confidence to 2dp
+        return newString.toString()
+    }
+
+
+
+    private fun getSingleIdent(identifiedString: ArrayList<String>, indexStart: Int) : ArrayList<String>{ //Returns an ArrayList, 3 long from the provided ArrayList
+        var orignalString = identifiedString
+        var newString = ArrayList<String>()
+        var counter = 0
+        var index = indexStart
+        for (item in orignalString){
+            if (counter <= 2) {
+                newString.add(counter, orignalString.get(index))
+                index++
+                counter++
+            }
+        }
+        return newString
+    }
+
+    //Show top three reccomendations
+    //Populate the static textfields
+    //hide the fields which are not used
+    //have an temp image for each plant
+    //redownload the identified plant
 
     private fun verifyLoggedIn(){ //Check if the User is already logged in, if not, return User to registerActivity
         val uid = FirebaseAuth.getInstance().uid
@@ -49,12 +144,4 @@ class IdentifiedActivity : AppCompatActivity() {
     }
 }
 
-class IdentifiedPlant: Item<ViewHolder>(){
-    override fun bind(viewHolder: ViewHolder, position: Int) {
-     //Will be called in the list for each object
-    }
 
-    override fun getLayout(): Int { //Renders each row
-        return R.layout.identifiedrow
-    }
-}
